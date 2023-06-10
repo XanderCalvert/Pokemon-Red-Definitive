2261684427303017d4ee72bd7e019fd6c2e909d4
Matt Calvert <xander.raymatt@gmail.com>
2023-06-11 00:04:44 +0100

    Adding comments to Route 1 encounters

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, PIDGEY
-	db  3, RATTATA
-	db  3, RATTATA
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, PIDGEY
-	db  3, PIDGEY
-	db  4, RATTATA
-	db  4, PIDGEY
-	db  5, PIDGEY
+    db 3,PIDGEY     ; 20%
+    db 3,RATTATA    ; 20%
+    db 3,RATTATA    ; 15%
+    db 2,RATTATA    ; 10%
+    db 2,PIDGEY     ; 10%
+    db 3,PIDGEY     ; 10%
+    db 3,PIDGEY     ; 5%
+    db 4,RATTATA    ; 5%
+    db 4,PIDGEY     ; 4%
+    db 5,PIDGEY     ; 1%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
