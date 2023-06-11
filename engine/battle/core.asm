748c0bd8bd7958baea08e15fc42341acc1857e5e
Matt Calvert <xander.raymatt@gmail.com>
2023-06-11 10:19:35 +0100

    changing the Crit math

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4608,7 +4608,7 @@ CriticalHitTest:
 	call GetMonHeader
 	ld a, [wMonHBaseSpeed]
 	ld b, a
-	srl b                        ; (effective (base speed/2))
+	; srl b                        ; (effective (base speed/2))
 	ldh a, [hWhoseTurn]
 	and a
 	ld hl, wPlayerMovePower
@@ -4622,34 +4622,39 @@ CriticalHitTest:
 	ret z                        ; do nothing if zero
 	dec hl
 	ld c, [hl]                   ; read move id
-	ld a, [de]
-	bit GETTING_PUMPED, a        ; test for focus energy
-	jr z, .noFocusEnergyUsed     ; Fixed the bug where it would reduce Crit Chance
-	sla b                        ; (effective (base speed/2)*2)
-	jr nc, .focusEnergyUsed
-	ld b, $ff                    ; cap at 255/256
-	jr .focusEnergyUsed
-.noFocusEnergyUsed
-	srl b
-.focusEnergyUsed
-	ld hl, HighCriticalMoves     ; table of high critical hit moves
+    ld hl, HighCriticalMoves     ; table of high critical hit moves
 .Loop
 	ld a, [hli]                  ; read move from move table
 	cp c                         ; does it match the move about to be used?
 	jr z, .HighCritical          ; if so, the move about to be used is a high critical hit ratio move
 	inc a                        ; move on to the next move, FF terminates loop
 	jr nz, .Loop                 ; check the next move in HighCriticalMoves
-	srl b                        ; /2 for regular move (effective (base speed / 2))
+	srl b                        ; /2 for regular move
 	jr .SkipHighCritical         ; continue as a normal move
 .HighCritical
 	sla b                        ; *2 for high critical hit moves
 	jr nc, .noCarry
 	ld b, $ff                    ; cap at 255/256
 .noCarry
-	sla b                        ; *4 for high critical move (effective (base speed/2)*8))
+	sla b                        ; *4 for high critical move
 	jr nc, .SkipHighCritical
 	ld b, $ff
 .SkipHighCritical
+	ld a, [de]
+	bit GETTING_PUMPED, a        ; test for focus energy
+	jr z, .noFocusEnergyUsed
+	sla b                        ; (effective (base speed*2))
+	jr nc, .focusEnergyUsed
+	ld b, $ff                    ; cap at 255/256
+    jr .focusEnergyUsed
+.noFocusEnergyUsed
+	srl b
+    jr .SkipFocusEnergy
+.focusEnergyUsed
+	sla b                        ; (effective ((base speed*2)*2))
+	jr nc, .noFocusEnergyUsed
+	ld b, $ff                    ; cap at 255/256
+.SkipFocusEnergy
  	ld a, b
  	inc a ; optimization of "cp $ff"
  	jr z, .guaranteedCriticalHit
@@ -4663,6 +4668,7 @@ CriticalHitTest:
 	ld a, $1
 	ld [wCriticalHitOrOHKO], a   ; set critical hit flag
 	ret
+
 
 INCLUDE "data/battle/critical_hit_moves.asm"
 
