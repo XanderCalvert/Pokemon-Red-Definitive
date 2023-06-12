3dae534eac4c4fccc40fbae09d48734adc217714
Matt Calvert <xander.raymatt@gmail.com>
2023-06-12 22:44:26 +0100

    Adding some more comments around the subroutine

--- scripts/HallOfFame.asm
+++ scripts/HallOfFame.asm
@@ -73,33 +73,34 @@ RLEMovement5a528:
 	db -1 ; end
 
 HallofFameRoomScript1:
-	ld a, [wSimulatedJoypadStatesIndex]
-	and a
-	ret nz
-	ld a, PLAYER_DIR_RIGHT
-	ld [wPlayerMovingDirection], a
-	ld a, $1
-	ldh [hSpriteIndex], a
-	call SetSpriteMovementBytesToFF
-	ld a, SPRITE_FACING_LEFT
-	ldh [hSpriteFacingDirection], a
-	call SetSpriteFacingDirectionAndDelay
-	call Delay3
-	xor a
-	ld [wJoyIgnore], a
-	inc a ; PLAYER_DIR_RIGHT
-	ld [wPlayerMovingDirection], a
-	ld a, $1
-	ldh [hSpriteIndexOrTextID], a
-	call DisplayTextID
-	ld a, $ff
-	ld [wJoyIgnore], a
-	ld a, HS_CERULEAN_CAVE_GUY ; Hide the guy outside of Cerulean Cave
-	ld [wMissableObjectIndex], a
-	predef HideObject
-	ld a, $2
-	ld [wHallOfFameCurScript], a
-	ret
+    ld a, [wSimulatedJoypadStatesIndex] ; Load the value at memory address wSimulatedJoypadStatesIndex into register A
+    and a ; Perform a bitwise AND operation on the value in register A with itself
+    ret nz ; Return if the previous operation did not result in zero (i.e., if wSimulatedJoypadStatesIndex is non-zero)
+    ld a, PLAYER_DIR_RIGHT ; Load the value representing the player's right direction into register A
+    ld [wPlayerMovingDirection], a ; Store the value of register A at memory address wPlayerMovingDirection (force the player to face right)
+    ld a, $1 ; Load 1 into register A
+    ldh [hSpriteIndex], a ; Load the value of register A into memory address hSpriteIndex (probably referring to a sprite to be moved)
+    call SetSpriteMovementBytesToFF ; Call a subroutine that presumably initializes sprite movement data
+    ld a, SPRITE_FACING_LEFT ; Load the value representing a sprite facing left into register A
+    ldh [hSpriteFacingDirection], a ; Store the value of register A at memory address hSpriteFacingDirection (force the sprite to face left)
+    call SetSpriteFacingDirectionAndDelay ; Call a subroutine that sets the sprite's facing direction and possibly imposes a delay
+    call Delay3 ; Call a subroutine that causes a short delay
+    xor a ; Perform a bitwise XOR on the value in register A with itself, resulting in 0
+    ld [wJoyIgnore], a ; Store the value of register A at memory address wJoyIgnore (effectively unblocking the joystick)
+    inc a ; Increment the value in register A by 1 (since A was 0, this will make A 1, i.e., PLAYER_DIR_RIGHT)
+    ld [wPlayerMovingDirection], a ; Store the value of register A at memory address wPlayerMovingDirection (force the player to face right)
+    ld a, $1 ; Load 1 into register A
+    ldh [hSpriteIndexOrTextID], a ; Load the value of register A into memory address hSpriteIndexOrTextID (probably to change the displayed sprite or text)
+    call DisplayTextID ; Call a subroutine to display the text whose ID was set above
+    ld a, $ff ; Load the value $ff into register A
+    ld [wJoyIgnore], a ; Store the value of register A at memory address wJoyIgnore (effectively ignoring joystick inputs)
+    ld a, HS_CERULEAN_CAVE_GUY ; Load the value representing the guy outside of Cerulean Cave into register A
+    ld [wMissableObjectIndex], a ; Store the value of register A at memory address wMissableObjectIndex (probably referring to the character to hide)
+    predef HideObject ; Call a predefined subroutine that hides the specified object
+    ld a, $2 ; Load 2 into register A
+    ld [wHallOfFameCurScript], a ; Store the value of register A at memory address wHallOfFameCurScript (probably indicating the current script to run in the Hall of Fame room)
+    ret ; Return from this subroutine
+
 
 HallOfFame_TextPointers:
 	dw HallofFameRoomText1
