924fdaad0884fe5a6b38b6db28a8e60f1f66f452
Matt Calvert <64328076+XanderCalvert@users.noreply.github.com>
2023-06-13 11:00:37 +0100

    Update CeladonMansion3F with comments

--- scripts/CeladonMansion3F.asm
+++ scripts/CeladonMansion3F.asm
@@ -1,95 +1,83 @@
+; Main entry point for the Celadon Mansion 3F script.
 CeladonMansion3F_Script:
-	jp EnableAutoTextBoxDrawing
+    jp EnableAutoTextBoxDrawing ; Jump to subroutine that enables automatic drawing of text boxes
 
+; Pointers to different dialogues.
 CeladonMansion3F_TextPointers:
-	dw ProgrammerText
-	dw GraphicArtistText
-	dw WriterText
-	dw DirectorText
-	dw GameFreakPCText1
-	dw GameFreakPCText2
-	dw GameFreakPCText3
-	dw GameFreakSignText
+    dw ProgrammerText    ; Dialogue with the programmer.
+    dw GraphicArtistText ; Dialogue with the graphic artist.
+    dw WriterText        ; Dialogue with the writer.
+    dw DirectorText      ; Dialogue with the director.
+    dw GameFreakPCText1  ; Dialogue for GameFreak PC option 1.
+    dw GameFreakPCText2  ; Dialogue for GameFreak PC option 2.
+    dw GameFreakPCText3  ; Dialogue for GameFreak PC option 3.
+    dw GameFreakSignText ; Dialogue for the GameFreak sign.
 
+; Below are different dialogues that can be triggered in Celadon Mansion 3F.
+
+; Dialogue with the programmer.
 ProgrammerText:
-	text_far _ProgrammerText
-	text_end
+    text_far _ProgrammerText ; Load the text from the programmer.
+    text_end                 ; Mark the end of the text.
 
+; Dialogue with the graphic artist.
 GraphicArtistText:
-	text_far _GraphicArtistText
-	text_end
+    text_far _GraphicArtistText ; Load the text from the graphic artist.
+    text_end                    ; Mark the end of the text.
 
+; Dialogue with the writer.
 WriterText:
-	text_far _WriterText
-	text_end
+    text_far _WriterText ; Load the text from the writer.
+    text_end             ; Mark the end of the text.
 
+; Dialogue with the director.
 DirectorText:
-	; loading the number of Pokemon owned in the Pokedex into hl
-	text_asm
-	ld hl, wPokedexOwned
-
-	; calculating the range of the Pokedex owned
-	ld b, wPokedexOwnedEnd - wPokedexOwned
-
-	; counting the number of set bits (owned Pokemon)
-	call CountSetBits
-
-	; loading the number of owned Pokemon into a
-	ld a, [wNumSetBits]
-
-	; subtracting one for Mew, who isn't counted
-	cp NUM_POKEMON - 1
-
-	; if we have all other Pokemon, go to .completed_dex
-	jr nc, .completed_dex
-
-	; if we don't have all Pokemon, load the Game Designer's text into hl
-	ld hl, .GameDesignerText
-	jr .done
-
+    text_asm                     ; Start of custom assembly code.
+    ld hl, wPokedexOwned         ; Load the address of the first Pokédex entry.
+    ld b, wPokedexOwnedEnd - wPokedexOwned ; Get the total number of Pokédex entries.
+    call CountSetBits             ; Count the number of owned Pokémon.
+    ld a, [wNumSetBits]          ; Load the count into register a.
+    cp NUM_POKEMON - 1           ; Compare with total number of Pokémon (excluding Mew).
+    jr nc, .completed_dex         ; If the number of owned Pokémon is not less, jump to completed dex dialogue.
+    ld hl, .GameDesignerText     ; Else, load address of game designer text.
+    jr .done                     ; Jump to .done.
 .completed_dex
-	; if we have all other Pokemon, load the Completed Dex text into hl
-	ld hl, .CompletedDexText
-
+    ld hl, .CompletedDexText      ; Load address of completed dex text.
 .done
-	; print the text pointed to by hl
-	call PrintText
+    call PrintText                ; Print the loaded text.
+    jp TextScriptEnd              ; Jump to the end of the text script.
 
-	; end the text script and return to the main game loop
-	jp TextScriptEnd
-
+; Dialogue for game designer.
 .GameDesignerText:
-	text_far _GameDesignerText
-	text_end
+    text_far _GameDesignerText ; Load the text from the game designer.
+    text_end                   ; Mark the end of the text.
 
+; Dialogue for completed Pokédex.
 .CompletedDexText:
-	text_far _CompletedDexText
-	text_promptbutton
+    text_far _CompletedDexText ; Load the text for completed dex.
+    text_promptbutton          ; Wait for button press before proceeding.
+    text_asm                   ; Start of custom assembly code.
+    callfar DisplayDiploma     ; Call subroutine to display the diploma for completing the Pokédex.
+    ld a, TRUE                 ; Load TRUE into register a.
+    ld [wDoNotWaitForButtonPressAfterDisplayingText], a ; Do not wait for a button press after displaying the text.
+    jp TextScriptEnd           ; Jump to the end of the text script.
 
-	; after the text prompt, display the diploma for completing the Pokedex
-	text_asm
-	callfar DisplayDiploma
-
-	; set the flag to not wait for button press after displaying text
-	ld a, TRUE
-	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
-	
-	; end the text script and return to the main game loop
-	jp TextScriptEnd
-
-
+; Dialogue for GameFreak PC option 1.
 GameFreakPCText1:
-	text_far _CeladonMansion3Text5
-	text_end
+    text_far _CeladonMansion3Text5 ; Load the text for GameFreak PC option 1.
+    text_end                       ; Mark the end of the text.
 
+; Dialogue for GameFreak PC option 2.
 GameFreakPCText2:
-	text_far _CeladonMansion3Text6
-	text_end
+    text_far _CeladonMansion3Text6 ; Load the text for GameFreak PC option 2.
+    text_end                       ; Mark the end of the text.
 
+; Dialogue for GameFreak PC option 3.
 GameFreakPCText3:
-	text_far _CeladonMansion3Text7
-	text_end
+    text_far _CeladonMansion3Text7 ; Load the text for GameFreak PC option 3.
+    text_end                       ; Mark the end of the text.
 
+; Dialogue for the GameFreak sign.
 GameFreakSignText:
-	text_far _CeladonMansion3Text8
-	text_end
+    text_far _CeladonMansion3Text8 ; Load the text for the GameFreak sign.
+    text_end                       ; Mark the end of the text.
