50f8ba28325117c63eb814bdfbc68db938e69d20
Matt Calvert <xander.raymatt@gmail.com>
2023-06-14 07:34:49 +0100

    Brainstorming how to impliment checks that you have caught 150 pokemon

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -14,8 +14,45 @@ IF DEF(_BLUE)
 ENDC
 	db 65, DITTO
 	db 63, DITTO
-	db 67, DITTO
+	db 67, MEW
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+; CeruleanCaveB1FWildMons:                  ; Defines the wild Pokémon encounter data for Cerulean Cave B1F
+; 	def_grass_wildmons 25 ; encounter rate ; Starts the encounter data for grass areas, with a 25% encounter rate
+; 	db 55, RHYDON                         ; 55 level RHYDON can be found in the grass
+; 	db 55, MAROWAK                        ; 55 level MAROWAK can be found in the grass
+; 	db 55, ELECTRODE                      ; 55 level ELECTRODE can be found in the grass
+; 	db 64, CHANSEY                        ; 64 level CHANSEY can be found in the grass
+; 	db 64, PARASECT                       ; 64 level PARASECT can be found in the grass
+; 	db 64, RAICHU                         ; 64 level RAICHU can be found in the grass
+; IF DEF(_RED)
+; 	db 57, ARBOK                          ; If the game version is Red, 57 level ARBOK can be found in the grass
+; ENDC
+; IF DEF(_BLUE)
+; 	db 57, SANDSLASH                      ; If the game version is Blue, 57 level SANDSLASH can be found in the grass
+; ENDC
+;     ; Loads the specific event flag that contains the bit for the EVENT_DIPLOMA_OBTAINED flag
+;     ld a, [wEventFlags + (EVENT_DIPLOMA_OBTAINED / 8)]
+;     ; Checks if the bit for the EVENT_DIPLOMA_OBTAINED flag is set (meaning the diploma was obtained)
+;     bit EVENT_DIPLOMA_OBTAINED % 8, a
+;     ; If the diploma was obtained (the bit was set), it jumps to the .DiplomaObtained label
+;     jr nz, .DiplomaObtained
+;     ; If the diploma was not obtained (the bit was not set), it continues to execute the following lines:
+;     db 65, DITTO                          ; 65 level DITTO can be found in the grass
+; 	db 63, DITTO                          ; 63 level DITTO can be found in the grass
+;     db 67, BULBASAUR                      ; 67 level BULBASAUR can be found in the grass
+;     ; After adding BULBASAUR to the encounter table, it jumps to the .EndGrassWildmons label
+;     jr .EndGrassWildmons
+; .DiplomaObtained
+;     ; If the diploma was obtained (it jumped here from the jr nz instruction), it executes the following lines:
+;     db 65, DITTO                          ; 65 level DITTO can be found in the grass
+; 	db 63, DITTO                          ; 63 level DITTO can be found in the grass
+;     db 67, MEW                            ; 67 level MEW can be found in the grass
+; .EndGrassWildmons
+; 	end_grass_wildmons                    ; Ends the grass encounter data block
+
+; 	def_water_wildmons 0 ; encounter rate ; Starts the encounter data for water areas, with a 0% encounter rate (meaning no encounters in water)
+; 	end_water_wildmons                    ; Ends the water encounter data block
--- engine/overworld/wild_mons.asm
+++ engine/overworld/wild_mons.asm
@@ -1,33 +1,75 @@
 LoadWildData::
-	ld hl, WildDataPointers
-	ld a, [wCurMap]
+	ld hl, WildDataPointers ; Load the base address of the WildDataPointers table
+	ld a, [wCurMap] ; Load the current map ID
 
 	; get wild data for current map
-	ld c, a
-	ld b, 0
-	add hl, bc
-	add hl, bc
-	ld a, [hli]
-	ld h, [hl]
-	ld l, a       ; hl now points to wild data for current map
-	ld a, [hli]
-	ld [wGrassRate], a
-	and a
-	jr z, .NoGrassData ; if no grass data, skip to surfing data
-	push hl
-	ld de, wGrassMons ; otherwise, load grass data
-	ld bc, $14
-	call CopyData
-	pop hl
-	ld bc, $14
-	add hl, bc
+	ld c, a ; Copy the current map ID to the c register
+	ld b, 0 ; Clear the b register
+	add hl, bc ; Offset the hl register by twice the current map ID (because each pointer is 2 bytes)
+	add hl, bc ; (we do this twice because there's no 'multiply by 2' instruction)
+	ld a, [hli] ; Load the first byte of the pointer at hl into a and increment hl
+	ld h, [hl] ; Load the second byte of the pointer into h
+	ld l, a ; Load the first byte of the pointer into l (hl now points to wild data for current map)
+	ld a, [hli] ; Load the encounter rate from the wild data into a
+	ld [wGrassRate], a ; Store the encounter rate
+	and a ; Perform a bitwise AND on a (checks if the encounter rate is 0)
+	jr z, .NoGrassData ; If the encounter rate is 0 (there's no grass data), skip to the water data
+	push hl ; Save the current address for later
+	ld de, wGrassMons ; Load the address of the grass encounter table
+	ld bc, $14 ; The size of the grass encounter table is 20 (0x14)
+	call CopyData ; Copy the grass encounter data to the table
+	pop hl ; Restore the address of the wild data
+	ld bc, $14 ; Offset hl by the size of the grass encounter table
+	add hl, bc ; Add the offset to hl
 .NoGrassData
-	ld a, [hli]
-	ld [wWaterRate], a
-	and a
-	ret z        ; if no water data, we're done
-	ld de, wWaterMons  ; otherwise, load surfing data
-	ld bc, $14
-	jp CopyData
+	ld a, [hli] ; Load the water encounter rate from the wild data into a
+	ld [wWaterRate], a ; Store the encounter rate
+	and a ; Perform a bitwise AND on a (checks if the encounter rate is 0)
+	ret z ; If the encounter rate is 0 (there's no water data), return
+	ld de, wWaterMons ; Load the address of the water encounter table
+	ld bc, $14 ; The size of the water encounter table is 20 (0x14)
+	jp CopyData ; Copy the water encounter data to the table (and return)
 
 INCLUDE "data/wild/grass_water.asm"
+
+
+; LoadWildData::
+;     ; Check if the player has obtained the diploma
+;     ld a, [wEventFlags + (EVENT_DIPLOMA_OBTAINED / 8)]
+;     bit EVENT_DIPLOMA_OBTAINED % 8, a
+;     jr nz, .DiplomaObtained
+;     ; If the diploma has not been obtained, load from the regular pointer list
+;     ld hl, WildDataPointers
+;     jr .LoadWildData
+; .DiplomaObtained:
+;     ; If the diploma has been obtained, load from the special pointer list
+;     ld hl, DiplomaWildDataPointers
+; .LoadWildData:
+;     ; The rest of the code is unchanged
+;     ld a, [wCurMap]
+;     ld c, a
+;     ld b, 0
+;     add hl, bc
+;     add hl, bc
+;     ld a, [hli]
+;     ld h, [hl]
+;     ld l, a       ; hl now points to wild data for current map
+;     ld a, [hli]
+;     ld [wGrassRate], a
+;     and a
+;     jr z, .NoGrassData ; if no grass data, skip to surfing data
+;     push hl
+;     ld de, wGrassMons ; otherwise, load grass data
+;     ld bc, $14
+;     call CopyData
+;     pop hl
+;     ld bc, $14
+;     add hl, bc
+; .NoGrassData
+;     ld a, [hli]
+;     ld [wWaterRate], a
+;     and a
+;     ret z        ; if no water data, we're done
+;     ld de, wWaterMons  ; otherwise, load surfing data
+;     ld bc, $14
+;     jp CopyData
