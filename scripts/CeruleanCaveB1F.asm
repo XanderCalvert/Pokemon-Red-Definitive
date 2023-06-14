3bb3cc5ef1eb09578a664700c6a81a9213d87c25
Matt Calvert <xander.raymatt@gmail.com>
2023-06-14 23:40:06 +0100

    Adding comments for Mewtwo

--- scripts/CeruleanCaveB1F.asm
+++ scripts/CeruleanCaveB1F.asm
@@ -1,38 +1,39 @@
 CeruleanCaveB1F_Script:
-	call EnableAutoTextBoxDrawing
-	ld hl, CeruleanCaveB1FTrainerHeaders
-	ld de, CeruleanCaveB1F_ScriptPointers
-	ld a, [wCeruleanCaveB1FCurScript]
-	call ExecuteCurMapScriptInTable
-	ld [wCeruleanCaveB1FCurScript], a
-	ret
+	call EnableAutoTextBoxDrawing   ; This function call will enable the automatic drawing of text boxes on the screen.
+	ld hl, CeruleanCaveB1FTrainerHeaders    ; Load the address of CeruleanCaveB1FTrainerHeaders into the hl register. This is where trainer information for Cerulean Cave is stored.
+	ld de, CeruleanCaveB1F_ScriptPointers   ; Load the address of CeruleanCaveB1F_ScriptPointers into the de register. This is where pointers to different script routines are stored.
+	ld a, [wCeruleanCaveB1FCurScript]   ; Load the value at the memory address wCeruleanCaveB1FCurScript into the a register. This value is used to determine which script to run.
+	call ExecuteCurMapScriptInTable     ; Call the function ExecuteCurMapScriptInTable, which runs the script indicated by the value in register a.
+	ld [wCeruleanCaveB1FCurScript], a   ; Store the value in the a register at the memory address wCeruleanCaveB1FCurScript, updating the current script to the one that was just run.
+	ret     ; Return from the current routine.
 
 CeruleanCaveB1F_ScriptPointers:
-	dw CheckFightingMapTrainers
-	dw DisplayEnemyTrainerTextAndStartBattle
-	dw EndTrainerBattle
+	dw CheckFightingMapTrainers     ; This is a pointer to the CheckFightingMapTrainers function.
+	dw DisplayEnemyTrainerTextAndStartBattle    ; This is a pointer to the DisplayEnemyTrainerTextAndStartBattle function.
+	dw EndTrainerBattle     ; This is a pointer to the EndTrainerBattle function.
 
 CeruleanCaveB1F_TextPointers:
-	dw MewtwoText
-	dw PickUpItemText
-	dw PickUpItemText
+	dw MewtwoText   ; This is a pointer to the MewtwoText function.
+	dw PickUpItemText   ; This is a pointer to the PickUpItemText function.
+	dw PickUpItemText   ; This is another pointer to the PickUpItemText function.
 
 CeruleanCaveB1FTrainerHeaders:
-	def_trainers
+	def_trainers    ; This defines the trainers present in Cerulean Cave.
+
 MewtwoTrainerHeader:
-	trainer EVENT_BEAT_MEWTWO, 0, MewtwoBattleText, MewtwoBattleText, MewtwoBattleText
-	db -1 ; end
+    trainer EVENT_BEAT_MEWTWO, 0, MewtwoBattleText, MewtwoBattleText, MewtwoBattleText      ; This defines a trainer header for Mewtwo, setting the associated event flag to EVENT_BEAT_MEWTWO.     ; The three occurrences of MewtwoBattleText are pointers to text scripts, respectively executed before, during, and after the battle.
+    db -1   ; This is an end marker, indicating the end of the trainer data.
 
 MewtwoText:
-	text_asm
-	ld hl, MewtwoTrainerHeader
-	call TalkToTrainer
-	jp TextScriptEnd
+    text_asm    ; This keyword indicates that following code will be assembly.
+    ld hl, MewtwoTrainerHeader  ; Load the address of MewtwoTrainerHeader into the hl register.
+    call TalkToTrainer  ; Call the TalkToTrainer function, which starts the interaction with the trainer.
+    jp TextScriptEnd    ; Jump to the TextScriptEnd label, ending the script.
 
 MewtwoBattleText:
-	text_far _MewtwoBattleText
-	text_asm
-	ld a, MEWTWO
-	call PlayCry
-	call WaitForSoundToFinish
-	jp TextScriptEnd
+    text_far _MewtwoBattleText      ; This indicates that the text for the Mewtwo battle is located elsewhere in memory and should be loaded from there.
+    text_asm    ; This keyword indicates that following code will be assembly.
+    ld a, MEWTWO    ; Load the identifier for Mewtwo's cry into the a register.
+    call PlayCry    ; Play Mewtwo's cry.
+    call WaitForSoundToFinish   ; Wait until the cry has finished playing before continuing.
+    jp TextScriptEnd    ; Jump to the TextScriptEnd label, ending the script.
--- text/CeruleanCaveB1F.asm
+++ text/CeruleanCaveB1F.asm
@@ -1,3 +1,24 @@
 _MewtwoBattleText::
 	text "Mew!@"
 	text_end
+
+; Mewtwo: I wasn't born a Pokémon, I was created; and my creators have used and betrayed me! So, I stand alone!
+
+; I see now that the circumstances of one's birth are irrelevant; it is what you do with the gift of life that determines who you are.
+
+; BrunosRoomTrainerHeader0:
+; 	trainer EVENT_BEAT_BRUNOS_ROOM_TRAINER_0, 0, BrunoBeforeBattleText, BrunoEndBattleText, BrunoAfterBattleText
+; 	db -1 ; end
+
+; BrunoEndBattleText
+
+; _BrunoEndBattleText::
+; 	text "Why?"
+; 	line "How could I lose?"
+; 	prompt
+
+; _BrunoAfterBattleText::
+; 	text "My job is done!"
+; 	line "Go face your next"
+; 	cont "challenge!"
+; 	done
